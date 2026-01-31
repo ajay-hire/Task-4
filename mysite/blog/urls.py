111322deb28d1253d9e4290208ed8a8532ad5fa5
Ajay Hire <ajayhire3141@gmail.com>
2026-01-31 15:15:10 +0530

    Task-6: Added PostForm and frontend post creation

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostCreateView, PostDetailView
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post-list'),
+    path('', PostListView.as_view(), name='post_list'),
+    path('add/', PostCreateView.as_view(), name='post_add'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,32 @@
-# from django.shortcuts import render
-
-# Create your views here.
-
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import ListView, DetailView
 from .models import Post
+from .forms import PostForm
 
+
+# List all blog posts (Task-5)
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
+
+
+# Show single post using dynamic URL (Task-6)
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+# Create new post using form (Task-6)
+class PostCreateView(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_form.html', {'form': form})
+
+    def post(self, request):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+        return render(request, 'blog/post_form.html', {'form': form})
